4cd56d648fa152dc2b09c7c7250771d23bde91c9
Marsha <marshatiisa35@gmail.com>
2023-06-27 16:22:29 -0400

    add text area form and button

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,12 +1,32 @@
 import React, {useState, useEffect} from 'react'
 import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig'
+import {ID} from 'appwrite'
 
 const Room = () => {
 
 const [messages, setMessages] = useState([])
+const [messageBody, setMessageBody] = useState('')
+
     useEffect(() =>{
         getMessages()
     }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        let payload = {
+            body: messageBody
+        }
+
+        let response = await databases.createDocument(
+            DATABASE_ID,
+            COLLECTION_ID_MESSAGES,
+            ID.unique(),
+            payload
+        )
+
+        setMessageBody('')
+    }
 
     const getMessages = async () => {
         const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES)
@@ -16,6 +36,21 @@ const [messages, setMessages] = useState([])
     return (
         <main className='container'>
             <div className='room--container'>
+
+                <form id='message--form'>
+                    <div>
+                        <textarea
+                            required
+                            maxLength='1000'
+                            placeholder='Say hey'
+                            onChange={(e)=>{setMessageBody(e.target.value)}}
+                            value={messageBody}
+                            ></textarea>
+                    </div>
+                    <div className='send-btn--wrapper'>
+                       <input className='btn btn--secondary' type='submit' value='Send'/>
+                    </div>
+                </form>
                 <div>
                     <div>
                         {messages.map(messages =>(
