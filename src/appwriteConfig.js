ee54a56ce8d0d93d0faa8709c82732e4cd9a3fb0
Marsha <marshatiisa35@gmail.com>
2023-06-27 15:25:00 -0400

    database setup

---
+++ src/appwriteConfig.js
@@ -0,0 +1,15 @@
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '649b28ee3ae88a522a0b'
+export const DATABASE_ID = '649b2e87342d0635163a'
+export const COLLECTION_ID_MESSAGES = '649b2e9c0341920b94a5'
+
+const client = new Client();
+
+// hide these later
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('649b28ee3ae88a522a0b');
+
+export const databases = new Databases(client) 
+export default client;
